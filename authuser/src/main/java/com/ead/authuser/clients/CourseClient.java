2ecf68f365f02da3153bc05fef12e7dda9971da3
JoseVitorDurante <josevitor_limma@hotmail.com>
2022-04-13 21:03:49 -0300

    Comunicação Síncrona - Inscrição de User em Course - Parte 1

--- authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -5,6 +5,7 @@ import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -20,7 +21,7 @@ import java.util.UUID;
 
 @Log4j2
 @Component
-public class UserClient {
+public class CourseClient {
 
     @Autowired
     RestTemplate restTemplate;
@@ -28,13 +29,15 @@ public class UserClient {
     @Autowired
     UtilsService utilsService;
 
-    String REQUEST_URI = "http://localhost:8082";
+
+    @Value("${ead.api.url.course}")
+    String REQUEST_URI;
 
     public Page<CourseDto> getAllCourseByUser(UUID userId, Pageable pageable) {
 
         List<CourseDto> searchResult = null;
 
-        String url = utilsService.createUrl(userId, pageable);
+        String url = REQUEST_URI + utilsService.createUrl(userId, pageable);
 
         try {
             ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
