16138ab272ab1292f0f808004c0fbd8d5db2bd15
JoseVitorDurante <josevitor_limma@hotmail.com>
2022-04-23 09:54:06 -0300

    Implementar Retry Resilience4j e Configurar Timeout

--- authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,6 +3,7 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -33,6 +35,7 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URI;
 
+    @Retry(name = "retryInstance", fallbackMethod = "retryfallback")
     public Page<CourseDto> getAllCourseByUser(UUID userId, Pageable pageable) {
 
         List<CourseDto> searchResult = null;
@@ -50,5 +53,11 @@ public class CourseClient {
         }
 
         return new PageImpl<>(searchResult);
+    }
+
+    public Page<CourseDto> retryfallback(UUID userId, Pageable pageable, Throwable t) {
+        log.error("Inside retry retryfallback, cause - {}", t, toString());
+        List<CourseDto> searchList = new ArrayList<>();
+        return new PageImpl<>(searchList);
     }
 }
