7b0a0b70daefa1e4ae1e9ff48e284e87342c8893
JoseVitorDurante <josevitor_limma@hotmail.com>
2022-05-04 20:22:21 -0300

    Criar Exchange Topic, definir Queue e Bindings

---
+++ notification/src/main/java/com/ead/notification/dtos/NotificationCommandDto.java
@@ -0,0 +1,34 @@
+package com.ead.notification.dtos;
+
+import java.util.UUID;
+
+public class NotificationCommandDto {
+
+    private String title;
+    private String message;
+    private UUID userID;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+}
