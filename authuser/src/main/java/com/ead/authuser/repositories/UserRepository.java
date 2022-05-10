f2485d876416097dc48fec019ba1e828cab5e993
JoseVitorDurante <josevitor_limma@hotmail.com>
2022-05-10 20:38:41 -0300

    Implementar UserDetails e UserDetailsService para Basic Auth com Database

--- authuser/src/main/java/com/ead/authuser/repositories/UserRepository.java
+++ authuser/src/main/java/com/ead/authuser/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.ead.authuser.repositories;
 
 import com.ead.authuser.models.UserModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
 import java.util.UUID;
 
 //não precisa anotar como repository por conta que o spring ja sabe que é por conta da extensão
@@ -12,4 +14,8 @@ public interface UserRepository extends JpaRepository<UserModel, UUID>, JpaSpeci
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    //EntityGraph carrega tambem as coleções que estão como lazy na entidade
+    @EntityGraph(attributePaths = "roles", type = EntityGraph.EntityGraphType.FETCH)
+    Optional<UserModel> findByUsername(String username);
 }
